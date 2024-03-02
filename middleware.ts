c7a02a3cd1075e8c5ee3b4f20c6dcba1c7a707dd
Nicholas <nicholas.griffin@bbc.co.uk>
2024-03-02 02:35:49 +0000

    chore: updating middleware config

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
 export default authMiddleware({});
